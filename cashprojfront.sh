7d061dd5fd37786bb421eea67e58a36326b597fb
TechGroup2020 <65551463+TechGroup2020@users.noreply.github.com>
2024-03-23 13:21:01 +0530

    Delete cashprojfront.sh
    deletet

--- cashprojfront.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker stop Cash-Proj-C-1
-docker rm Cash-Proj-C-1
-docker build -t cash-proj-f .
-docker run -i --name Cash-Proj-C-1 --network=gomatha --ip 192.168.1.60  -p 2727:2727  cash-proj-f /bin/bash
-docker run -it --name Cash-Proj-C-1 cash-proj-f /bin/bash
-docker start Cash-Proj-C-1
-docker exec Cash-Proj-C-1 ./startup.sh
